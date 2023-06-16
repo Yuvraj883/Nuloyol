4c8c832a41424bf219bbbe2e3479abcbec9f2d1d
Yuvraj <kingyuvi88@gmail.com>
2023-06-16 14:57:14 +0530

    Added about paragraph.

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -4,14 +4,14 @@ const AboutSection = () => {
     return (
         <>
             <div className='about-section'>
-               
-            <section className='img'>
-                    <img src="./images/AboutImg1.svg" alt="What we do"/>
+
+                <section className='img'>
+                    <img src="./images/AboutImg1.svg" alt="What we do" />
                     <div className='text'>
                         <h1>What we do</h1>
                         <h3>Learn what we really do</h3>
                         <p className='p-right'>
-                        Your sales team will love you because you provide them with the tools they need to work efficiently, close deals quickly, and achieve success.
+                            Your sales team will love you because you provide them with the tools they need to work efficiently, close deals quickly, and achieve success.
                         </p>
                     </div>
                 </section>
@@ -21,23 +21,27 @@ const AboutSection = () => {
                         <h1>How we do</h1>
                         <h3>Learn how we do that</h3>
                         <p className='p-left'>
-                        Your sales team will love you because you provide them with the tools they need to work efficiently, close deals quickly, and achieve success.
+                            Your sales team will love you because you provide them with the tools they need to work efficiently, close deals quickly, and achieve success.
                         </p>
                     </div>
-                    <img src="./images/AboutImg2.svg" alt="How we do that"/>
+                    <img src="./images/AboutImg2.svg" alt="How we do that" />
 
                 </section>
 
                 <section className='img'>
-                    <img src="./images/AboutImg3.svg" alt="What we do"/>
+                    <img src="./images/AboutImg3.svg" alt="What we do" />
                     <div className='text'>
                         <h1>Who we are</h1>
                         <h3>Learn who your players really are</h3>
                         <p className='p-right'>
-                        Your sales team will love you because you provide them with the tools they need to work efficiently, close deals quickly, and achieve success.
+                            Your sales team will love you because you provide them with the tools they need to work efficiently, close deals quickly, and achieve success.
                         </p>
                     </div>
                 </section>
+                <p className='about-p'>
+                    <h1>Self-funded, profitable and proud.</h1>
+                    Our core values that we live and breathe everyday.
+                </p>
             </div>
         </>
     )
