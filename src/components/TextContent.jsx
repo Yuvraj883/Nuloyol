a1391326925174dc99f208707e061a3187618e88
Yuvraj <kingyuvi88@gmail.com>
2023-06-16 13:41:13 +0530

    Completed TextContent section design for PC

---
+++ src/components/TextContent.jsx
@@ -0,0 +1,14 @@
+import './TextContent.css';
+const TextContent = () => {
+    return (
+        <>
+            <article className="text-content">
+                A centralised revenue hub for qoutes,
+                contracts, subscription management, and billing
+
+            </article>
+
+        </>
+    )
+}
+export default TextContent;
