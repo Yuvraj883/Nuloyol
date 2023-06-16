d85d3bffeb70c3831d7c2203e9a319b07974fb31
Yuvraj <kingyuvi88@gmail.com>
2023-06-16 17:36:40 +0530

    SocialMediaSection design completed for the PC

--- src/components/SocialMediaSection.jsx
+++ src/components/SocialMediaSection.jsx
@@ -1,10 +1,23 @@
-import './SocialMediaSection.jsx'; 
+import './SocialMediaSection.css';
 
-const SocialMediaSection = ()=>{
-    return(
+const SocialMediaSection = () => {
+    return (
         <>
-        
-        
+            <h1 className='social-heading'>Stay Connect With Us</h1>
+            <div className='social-media-section'>
+                <img src="./images/Twitter1.svg" alt="twitter icon" />
+                <img src="./images/Insta1.svg" alt="insta icon" />
+
+                <img src="./images/faceBook1.svg" alt="fb icon" />
+                <img src="./images/Insta1.svg" alt="insta icon" />
+                <img src="./images/Twitter1.svg" alt="twitter icon" />
+
+
+                <img src="./images/YouTube1.svg" alt="youtube" />
+
+
+            </div>
+
         </>
     )
 }
