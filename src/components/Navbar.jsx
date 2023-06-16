e11856e2e057ed033f6d6460681a6fdbe3ed64ba
Yuvraj <kingyuvi88@gmail.com>
2023-06-16 12:16:05 +0530

    PC design completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,25 @@
-const Navbar = ()=>{
+import './Navbar.css'
+const Navbar=()=> {
+    return (
+        <>
+            <nav className="navbar">
+            <span className="title">
+                NULOYAL
+            </span>
+            <div>
+                <ul >
+                    <li>Platform</li>
+                    <li>Customer</li>
+                    <li>Learning Center</li>
+                    <li>Company</li>
+                    <li>Pricing</li>
+                </ul>
+            </div>
+            <button className='request-demo'>Request Demo</button>
+            </nav>
+        </>
+
+    )
 
 }
 export default Navbar;
