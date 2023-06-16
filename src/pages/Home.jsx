68ff394d9a1f6862cd8735439d5015c908be8fed
Yuvraj <kingyuvi88@gmail.com>
2023-06-16 13:22:17 +0530

    Imported HeroSection and TextContent component into the page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import TextContent from "../components/TextContent";
 
-const Home = ()=>{
-    <Navbar/>
+const Home=()=> {
+    return (
+        <>
+            <Navbar/>
+            <HeroSection/>
+            <TextContent/>
+        </>
+    )
 
 }
 export default Home; 
