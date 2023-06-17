e6f0f829036ff4a19caa52d95a8126125a217015
Yuvraj <kingyuvi88@gmail.com>
2023-06-18 02:07:40 +0530

    Imported Navbar TextContent and Footer component

--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -1,7 +1,15 @@
+import Navbar from "../components/Navbar";
+import TextContent from "../components/TextContent";
+import Footer from "../components/Footer";
+
 
 const Company = ()=>{
     return(
         <>
+        <Navbar/>
+        <TextContent/>
+        <Footer/>
+
         
         </>
     )
