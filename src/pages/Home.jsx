b4ed58d72a7faff646c54728d12fc37da2e85cda
Yuvraj <kingyuvi88@gmail.com>
2023-06-17 09:33:57 +0530

    Added SocialMediaSection.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,17 @@ import HeroSection from "../components/HeroSection";
 import TextContent from "../components/TextContent";
 import AboutSection from "../components/AboutSection";
 import Footer from "../components/Footer";
+import SocialMediaSection from "../components/SocialMediaSection";
 
 const Home=()=> {
+    console.log("Social media section");
     return (
         <>
             <Navbar/>
             <HeroSection/>
             <TextContent/>
             <AboutSection/>
-            
+            <SocialMediaSection/>            
 
             <Footer/>
             
