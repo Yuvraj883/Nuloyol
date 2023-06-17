43c0b042b955da6d9c12b4c5c1a3f753406510c3
Yuvraj <kingyuvi88@gmail.com>
2023-06-18 01:44:19 +0530

    TryNuloyal section design completed for PC

--- src/components/TryNuloyal.jsx
+++ src/components/TryNuloyal.jsx
@@ -3,7 +3,29 @@ import './TryNuloyal.css';
 const TryNuloyal = ()=>{
     return(
         <>
-        
+        <div className='try-nuloyal'>
+            <div className='col-1'>
+                <h1>Try NULOYAL for free</h1>
+                <p>Put our CRM software to the test so you're confident it's the right fit.</p>
+                <div className='buttons-div'>
+                    <button className="primary">Request Demo</button>
+                    <button className='secondary'>Join Now</button>
+                </div>
+                <section className='free-section'>
+                    <span><b>&#x2713;</b> Free 14-day trial</span>
+                    <span><b>&#x2713;</b> No credit card required</span>
+                    <span><b>&#x2713;</b> Easy to setup</span>
+                    <span><b>&#x2713;</b> Free 14-day trial</span>
+                    <span><b>&#x2713;</b> No credit card required</span>
+                    <span><b>&#x2713;</b> Easy to setup</span>
+                    
+
+                </section>
+            </div>
+            <div className='img-div' >
+                <img src='./images/TryNuloyal.svg' alt="Nuloyal"/>
+            </div>
+        </div>
         </>
     )
 }
