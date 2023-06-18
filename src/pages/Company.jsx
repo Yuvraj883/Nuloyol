4b433fc9dbdc66632ca6e725e8ddcbd922dd7e35
Yuvraj <kingyuvi88@gmail.com>
2023-06-18 17:01:12 +0530

    Imported formsection to the company page.

--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -1,7 +1,9 @@
 import Navbar from "../components/Navbar";
 import TextContent from "../components/TextContent";
 import SocialMediaSection from "../components/SocialMediaSection";
+import TryNuloyal from "../components/TryNuloyal";
 import Footer from "../components/Footer";
+import FormSection from "../components/FormSection";
 
 
 
@@ -9,8 +11,10 @@ const Company = ()=>{
     return(
         <>
         <Navbar/>
+        <FormSection/>
         <TextContent/>
         <SocialMediaSection/>
+        <TryNuloyal/>
         <Footer/>
 
         
