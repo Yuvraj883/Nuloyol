8e4710045f6c81ca923d27c867c3706cdb49dba8
Yuvraj <kingyuvi88@gmail.com>
2023-06-18 20:54:18 +0530

    Formsection design completed for PC

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -1,8 +1,75 @@
-import './FormSectioin.css'; 
+import './FormSection.css';
 
-const FormSection = ()=>{
-    return(
+const FormSection = () => {
+    return (
         <>
+            <div className='main'>
+                <div className='explore-area'>
+                    <h1>Explore how Nuloyal can work for you</h1>
+                    <p>Streamline your quote-to-revenue process. Issue proposals, close deals, manage contracts, and automate your subscriptions and billing in one unified platform.</p>
+                    <p className='subscript'>Trusted by 50% of fortune 1000</p>
+                    <div className='contact-info'>
+                    <div className="flex">
+                                <img src="./images/Phone.svg" alt="phone-icon" />
+                                <div>
+                                    <p className='contact-info-p'>PHONE </p>
+
+                                    <span>111 1111 111</span>
+                                </div>
+                            </div>
+                            <div className="flex">
+                                <img src="./images/Email.svg" alt="phone-icon" />
+                                <div>
+                                    <p className='contact-info-p'>E-MAIL </p>
+
+                                    <span>info@company.com</span>
+                                </div>
+                            </div>
+                            <div className="flex">
+                                <img src="./images/Email.svg" alt="phone-icon" />
+                                <div>
+                                    <p className='contact-info-p'>HELPDESK </p>
+
+                                    <span>https://helpdesk.com</span>
+                                </div>
+                            </div>
+                    </div>
+                      
+                </div>
+
+                <div className='form'>
+                    <form >
+                        <label>
+                            Full Name
+                        </label>
+                        <input type='text' placeholder='Alex Smith' />
+                        <label>Work Email</label>
+                        <input type='email' placeholder='alex@email.com' />
+                        <div className='phone-date'>
+                            <div>
+                                <label >Phone number</label>
+                                <input type="tel" placeholder='999999999' />
+                            </div>
+
+                            <div>
+                                <label>Date</label>
+                                <input type='date' placeholder='DD/MM/YYYY' />
+
+
+                            </div>
+
+
+                        </div>
+                        <label>How can we help?</label>
+                        <textarea type='text' />
+                        <input className='submit-btn' type="submit" />
+                        <p>You verify that this info is correct</p>
+
+                    </form>
+
+                </div>
+
+            </div>
         </>
     )
 }
