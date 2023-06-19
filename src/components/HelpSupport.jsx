4fa1dc2f4d1f7a62919f2ea8321fb630d37bc0ac
Yuvraj <kingyuvi88@gmail.com>
2023-06-19 23:58:23 +0530

    Completed design for PC

--- src/components/HelpSupport.jsx
+++ src/components/HelpSupport.jsx
@@ -3,7 +3,82 @@ import './HelpSupport.css';
 const HelpSupport = ()=>{
     return(
         <>
-        <div></div>
+        <div className="help-support">
+            <p className="top-p">Have any questions ? we'd love to hear from you</p>
+          <section className="help-support-cards">
+            <div>
+                <hr className=""/>
+                <div className="help-support-card">
+                    <h1>Press</h1>
+                    <p>Streamline your quote-to-revenue process. Issue proposals, close deals, manage contracts, and automate your subscriptions and billing in one unified platform.</p>
+                    <div>
+                    <div className="border-padding ">
+                                <img src="./images/Email.svg" alt="phone-icon" />
+                                <div>
+                                    <p className='contact-info-p'>HELPDESK </p>
+
+                                    <span>https://helpdesk.com</span>
+                                </div>
+                        </div>
+                        
+                        
+                    </div>
+                    
+
+                </div>
+
+            </div>
+
+            <div>
+                <hr className="second-hr"/>
+                <div className="help-support-card second-card">
+                    <h1>Help & support</h1>
+                    <p>Streamline your quote-to-revenue process. Issue proposals, close deals, manage contracts, and automate your subscriptions and billing in one unified platform.</p>
+                    <div>
+                    <div className=" border-padding second-flex1">
+                                <img src="./images/Email.svg" alt="phone-icon" />
+                                <div>
+                                    <p className='contact-info-p'>HELPDESK </p>
+
+                                    <span>https://helpdesk.com</span>
+                                </div>
+                        </div>
+                        
+                        
+                    </div>
+                    
+
+                </div>
+
+            </div>
+
+            <div>
+                <hr className=""/>
+                <div className="help-support-card">
+                    <h1>sales</h1>
+                    <p>Streamline your quote-to-revenue process. Issue proposals, close deals, manage contracts, and automate your subscriptions and billing in one unified platform.</p>
+                    <div>
+                    <div className=" border-padding">
+                                <img src="./images/Email.svg" alt="phone-icon" />
+                                <div>
+                                    <p className='contact-info-p'>HELPDESK </p>
+
+                                    <span>https://helpdesk.com</span>
+                                </div>
+                        </div>
+                        
+                        
+                    </div>
+                    
+
+                </div>
+
+            </div>
+
+          </section>
+
+
+        </div>
         </>
     )
 }
