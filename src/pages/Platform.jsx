ba932d8f5a117cec463fd836dc8dc8a63f1263ff
Yuvraj <kingyuvi88@gmail.com>
2023-06-20 00:20:45 +0530

    Imported Footer, HeroSection, AboutSection and TextContent

--- src/pages/Platform.jsx
+++ src/pages/Platform.jsx
@@ -1,10 +1,19 @@
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer'; 
+import HeroSection from '../components/HeroSection'; 
+import AboutSection from '../components/AboutSection'; 
+import TextContent from '../components/TextContent';
+
 
 
 const Platform = ()=>{
     return(
         <>
         <Navbar/>
+        <HeroSection/>
+        <AboutSection/>
+        <TextContent/>
+        <Footer/>
         
         </>
     )
