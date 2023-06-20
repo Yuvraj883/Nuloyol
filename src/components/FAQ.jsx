11da3da2e28c6b7c0bbb1454d2872d6289cced91
Yuvraj <kingyuvi88@gmail.com>
2023-06-21 02:02:31 +0530

    Logical part implemented

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,20 +1,95 @@
+import { useState } from 'react';
 import './FAQ.css';
 
+
 const FAQ = () => {
+    const [question1, setQuestion1] = useState('true');
+    const [question2, setQuestion2] = useState('false');
+    const [question3, setQuestion3] = useState('false');
+    const [question4, setQuestion4] = useState('false');
+    const [question5, setQuestion5] = useState('false');
+
+   
+    const handleSetQuestion1 = ()=>{
+        setQuestion1('true');
+        setQuestion2('false');
+        setQuestion3('false');
+        setQuestion4('false');
+        setQuestion5('false');
+
+    }
+
+
+    const handleSetQuestion2 = ()=>{
+        setQuestion1('false');
+        setQuestion2('true');
+        setQuestion3('false');
+        setQuestion4('false');
+        setQuestion5('false');
+
+    } 
+
+    const handleSetQuestion3 = ()=>{
+        setQuestion1('false');
+        setQuestion2('false');
+        setQuestion3('true');
+        setQuestion4('false');
+        setQuestion5('false');
+
+    }
+
+    const handleSetQuestion4 = ()=>{
+        setQuestion1('false');
+        setQuestion2('false');
+        setQuestion3('false');
+        setQuestion4('true');
+        setQuestion5('false');
+
+    }
+
+    const handleSetQuestion5 = ()=>{
+        setQuestion1('false');
+        setQuestion2('false');
+        setQuestion3('false');
+        setQuestion4('false');
+        setQuestion5('true');
+
+    }
+
+
     return (
         <>
             <div className='FAQ'>
                 <div className='FAQ-questions'>
-                    <div className='question'>What is Chowdeck?</div>
-                    <div className='question'>What locations do we currently deliver to?</div>
-                    <div className='question'>What is Chowdeck wallet?</div>
-                    <div className='question'>What is Chowscore?</div>
-                    <div className='question'>What is Service fee?</div>
+                    <h1>FAQs.</h1>
+                    <div className={question1==='true'?'question active':'question'} onClick={handleSetQuestion1}>What is Chowdeck?</div>
+                    <div className={question2==='true'?'question active':'question'}  onClick={handleSetQuestion2}>What locations do we currently deliver to?</div>
+                    <div className={question3==='true'?'question active':'question'} onClick={handleSetQuestion3}>What is Chowdeck wallet?</div>
+                    <div className={question4==='true'?'question active':'question'} onClick={handleSetQuestion4}>What is Chowscore?</div>
+                    <div className={question5==='true'?'question active':'question'} onClick={handleSetQuestion5}>What is Service fee?</div>
                   
 
                 </div>
                 <div className='FAQ-answers'>
-                    <div className='answer'>
+                    <h1>Ans.</h1>
+                    
+                    <div className={question1==='true'?'answer active ':'hidden'}>
+                        1.
+                        <p>Chowdeck is a technology company that provides logistics services to both vendors and consumers. This potentially allows food vendors to deliver meals seamlessly while also providing consumers with an easy platform to order meals from their favourite restaurants in their city.</p>
+                    </div>
+                    <div className={question2==='true'?'answer active':'hidden'}>
+                        2.
+                        <p>Chowdeck is a technology company that provides logistics services to both vendors and consumers. This potentially allows food vendors to deliver meals seamlessly while also providing consumers with an easy platform to order meals from their favourite restaurants in their city.</p>
+                    </div><div className={question3==='true'?'answer active':'hidden'}>
+                        3.
+                        <p>Chowdeck is a technology company that provides logistics services to both vendors and consumers. This potentially allows food vendors to deliver meals seamlessly while also providing consumers with an easy platform to order meals from their favourite restaurants in their city.</p>
+                    </div>
+                    <div className={question4==='true'?'answer active':'hidden'}>
+                        4.
+                        <p>Chowdeck is a technology company that provides logistics services to both vendors and consumers. This potentially allows food vendors to deliver meals seamlessly while also providing consumers with an easy platform to order meals from their favourite restaurants in their city.</p>
+                    </div>
+                    <div className={question5==='true'?'answer active':'hidden'}>
+                        5.
                         <p>Chowdeck is a technology company that provides logistics services to both vendors and consumers. This potentially allows food vendors to deliver meals seamlessly while also providing consumers with an easy platform to order meals from their favourite restaurants in their city.</p>
                     </div>
                 </div>
