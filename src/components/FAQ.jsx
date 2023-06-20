7eaa06367f434b42ca37d8db2301d0c220079202
Yuvraj <kingyuvi88@gmail.com>
2023-06-21 02:18:50 +0530

    Logical part completed

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -9,8 +9,8 @@ const FAQ = () => {
     const [question4, setQuestion4] = useState('false');
     const [question5, setQuestion5] = useState('false');
 
-   
-    const handleSetQuestion1 = ()=>{
+
+    const handleSetQuestion1 = () => {
         setQuestion1('true');
         setQuestion2('false');
         setQuestion3('false');
@@ -20,16 +20,16 @@ const FAQ = () => {
     }
 
 
-    const handleSetQuestion2 = ()=>{
+    const handleSetQuestion2 = () => {
         setQuestion1('false');
         setQuestion2('true');
         setQuestion3('false');
         setQuestion4('false');
         setQuestion5('false');
 
-    } 
+    }
 
-    const handleSetQuestion3 = ()=>{
+    const handleSetQuestion3 = () => {
         setQuestion1('false');
         setQuestion2('false');
         setQuestion3('true');
@@ -38,7 +38,7 @@ const FAQ = () => {
 
     }
 
-    const handleSetQuestion4 = ()=>{
+    const handleSetQuestion4 = () => {
         setQuestion1('false');
         setQuestion2('false');
         setQuestion3('false');
@@ -47,7 +47,7 @@ const FAQ = () => {
 
     }
 
-    const handleSetQuestion5 = ()=>{
+    const handleSetQuestion5 = () => {
         setQuestion1('false');
         setQuestion2('false');
         setQuestion3('false');
@@ -62,34 +62,54 @@ const FAQ = () => {
             <div className='FAQ'>
                 <div className='FAQ-questions'>
                     <h1>FAQs.</h1>
-                    <div className={question1==='true'?'question active':'question'} onClick={handleSetQuestion1}>What is Chowdeck?</div>
-                    <div className={question2==='true'?'question active':'question'}  onClick={handleSetQuestion2}>What locations do we currently deliver to?</div>
-                    <div className={question3==='true'?'question active':'question'} onClick={handleSetQuestion3}>What is Chowdeck wallet?</div>
-                    <div className={question4==='true'?'question active':'question'} onClick={handleSetQuestion4}>What is Chowscore?</div>
-                    <div className={question5==='true'?'question active':'question'} onClick={handleSetQuestion5}>What is Service fee?</div>
-                  
+                    <div className={question1 === 'true' ? 'question active' : 'question'} onClick={handleSetQuestion1}>What is Chowdeck?
+                        <img className={question1 === 'true' ? '' : 'hidden'} src='../images/YellowStar.svg' alt="star-icon" />
+                    </div>
+                    <div className={question2 === 'true' ? 'question active' : 'question'} onClick={handleSetQuestion2}>What locations do we currently deliver to?
+                        <img className={question2 === 'true' ? '' : 'hidden'} src='../images/YellowStar.svg' alt="star-icon" />
+                    </div>
+                    <div className={question3 === 'true' ? 'question active' : 'question'} onClick={handleSetQuestion3}>What is Chowdeck wallet?
+                        <img className={question3 === 'true' ? '' : 'hidden'} src='../images/YellowStar.svg' alt="star-icon" />
+                    </div>
+                    <div className={question4 === 'true' ? 'question active' : 'question'} onClick={handleSetQuestion4}>What is Chowscore?
+                        <img className={question4 === 'true' ? '' : 'hidden'} src='../images/YellowStar.svg' alt="star-icon" />
+                    </div>
+                    <div className={question5 === 'true' ? 'question active' : 'question'} onClick={handleSetQuestion5}>What is Service fee?
+                        <img className={question5 === 'true' ? '' : 'hidden'} src='../images/YellowStar.svg' alt="star-icon" />
+                    </div>
+
 
                 </div>
                 <div className='FAQ-answers'>
                     <h1>Ans.</h1>
-                    
-                    <div className={question1==='true'?'answer active ':'hidden'}>
+
+                    <div className={question1 === 'true' ? 'answer active ' : 'hidden'}>
                         1.
+                        <img className={question1 === 'true' ? '' : 'hidden'} src='../images/WhiteStar.svg' alt="star-icon" />
+
                         <p>Chowdeck is a technology company that provides logistics services to both vendors and consumers. This potentially allows food vendors to deliver meals seamlessly while also providing consumers with an easy platform to order meals from their favourite restaurants in their city.</p>
                     </div>
-                    <div className={question2==='true'?'answer active':'hidden'}>
+                    <div className={question2 === 'true' ? 'answer active' : 'hidden'}>
                         2.
+                        <img className={question2 === 'true' ? '' : 'hidden'} src='../images/WhiteStar.svg' alt="star-icon" />
+
                         <p>Chowdeck is a technology company that provides logistics services to both vendors and consumers. This potentially allows food vendors to deliver meals seamlessly while also providing consumers with an easy platform to order meals from their favourite restaurants in their city.</p>
-                    </div><div className={question3==='true'?'answer active':'hidden'}>
+                    </div><div className={question3 === 'true' ? 'answer active' : 'hidden'}>
                         3.
+                        <img className={question3 === 'true' ? '' : 'hidden'} src='../images/WhiteStar.svg' alt="star-icon" />
+
                         <p>Chowdeck is a technology company that provides logistics services to both vendors and consumers. This potentially allows food vendors to deliver meals seamlessly while also providing consumers with an easy platform to order meals from their favourite restaurants in their city.</p>
                     </div>
-                    <div className={question4==='true'?'answer active':'hidden'}>
+                    <div className={question4 === 'true' ? 'answer active' : 'hidden'}>
                         4.
+                        <img className={question4 === 'true' ? '' : 'hidden'} src='../images/WhiteStar.svg' alt="star-icon" />
+
                         <p>Chowdeck is a technology company that provides logistics services to both vendors and consumers. This potentially allows food vendors to deliver meals seamlessly while also providing consumers with an easy platform to order meals from their favourite restaurants in their city.</p>
                     </div>
-                    <div className={question5==='true'?'answer active':'hidden'}>
+                    <div className={question5 === 'true' ? 'answer active' : 'hidden'}>
                         5.
+                        <img className={question5 === 'true' ? '' : 'hidden'} src='../images/WhiteStar.svg' alt="star-icon" />
+
                         <p>Chowdeck is a technology company that provides logistics services to both vendors and consumers. This potentially allows food vendors to deliver meals seamlessly while also providing consumers with an easy platform to order meals from their favourite restaurants in their city.</p>
                     </div>
                 </div>
