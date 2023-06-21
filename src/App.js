2a8e464d9e34f8629b96cb55d954d57e956e253d
Yuvraj <kingyuvi88@gmail.com>
2023-06-21 19:38:48 +0530

    Implemented React router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import './App.css';
 import Home from './pages/Home';
 import Company from './pages/Company';
 import Platform from './pages/Platform';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <>
     <div className="app">
+    <BrowserRouter>
+      <Routes>
+        <Route 
+        exact path="/" element={<Home/>}
+        />
+      <Route 
+         path="company" element={<Company/>}
+        />
+          <Route 
+         path="platform" element={<Platform/>}
+        />
+      </Routes>
+    </BrowserRouter>
+    {/* <Company/>
     <Home/>
-    <Platform/>
-    <Company/>
+    <Platform/> */}
     
     </div>
     </>
