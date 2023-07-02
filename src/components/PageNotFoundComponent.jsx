62b49df9df25c7456ef63ce6461fda5ab880fc87
Yuvraj <kingyuvi88@gmail.com>
2023-07-02 20:52:57 +0530

    Completed design for PageNotFoundComponent

---
+++ src/components/PageNotFoundComponent.jsx
@@ -0,0 +1,14 @@
+import './PageNotFoundComponent.css';
+const PageNotFoundComponent = ()=>{
+    return (
+        <>
+         <div className="main-div">
+        <p className="">Error 404</p>
+        <p className="">This page doesn't exist.</p>
+
+        </div>
+        </>
+    )
+};
+ 
+export default PageNotFoundComponent;
