701162a184337c5d450280b7f20edd4044d3dd17
Yuvraj <kingyuvi88@gmail.com>
2023-07-02 20:19:16 +0530

    Create PageNotFound page

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+const PageNotFound = ()=>{
+    return(
+        <>
+        <div className="flex flex-rox justify-center items-center">
+        <p className="text-4xl font-bold">Error 404</p>
+        <p className="text-lg font-bold">Page doesn't exist</p>
+
+        </div>
+        </>
+    )
+}
+export default PageNotFound;
