dd205e764da565a171dd059122fdefeebc155b4d
Yuvraj <kingyuvi88@gmail.com>
2023-07-02 20:38:27 +0530

    Imported page not found compponent

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,11 +1,8 @@
+import PageNotFoundComponent from "../components/PageNotFoundComponent";
 const PageNotFound = ()=>{
     return(
         <>
-        <div className="flex flex-rox justify-center items-center">
-        <p className="text-4xl font-bold">Error 404</p>
-        <p className="text-lg font-bold">Page doesn't exist</p>
-
-        </div>
+       <PageNotFoundComponent/>
         </>
     )
 }
